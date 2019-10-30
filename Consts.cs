7ee45e4ef9f66789e62fc72ea8d45af20570bfee
conradylx <konrad.bakowsky@gmail.com>
2019-10-30 20:51:26 +0100

    Initial commit

---
+++ Consts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForecastApp.Config
+{
+    public static class Constants
+    {
+        public const string OPEN_WEATHER_APPID = "4945b45d59bb10a32585892cdd9dc742";
+    }
+}
---
+++ Controllers/ForecastAppController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ForecastApp.Models;
+using ForecastApp.OpenWeatherModels;
+using ForecastApp.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForecastApp.Controllers
+{
+    public class ForecastAppController : Controller
+    {
+        private readonly IForecastRepository _forecastRepository;
+
+        public ForecastAppController(IForecastRepository forecastAppRepo)
+        {
+            _forecastRepository = forecastAppRepo;
+        }
+        public IActionResult SearchCity()
+        {
+            var viewModel = new SearchCity();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult SearchCity(SearchCity model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("City", "ForecastApp", new { city = model.CityName });
+            }
+            return View(model);
+        }
+
+        public IActionResult City(string city)
+        {
+            WeatherResponse weatherResponse = _forecastRepository.GetForecast(city);
+            City viewModel = new City();
+
+            if (weatherResponse != null)
+            {
+                viewModel.Name = weatherResponse.Name;
+                viewModel.Humidity = weatherResponse.Main.Humidity;
+                viewModel.Pressure = weatherResponse.Main.Pressure;
+                viewModel.Temp = weatherResponse.Main.Temp;
+                viewModel.Weather = weatherResponse.Weather[0].Main;
+                viewModel.Wind = weatherResponse.Wind.Speed;
+            }
+            return View(viewModel);
+        }
+    }
+}
